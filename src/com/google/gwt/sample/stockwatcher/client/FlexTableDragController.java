435ede0a16ae648de915e4dd71ac8baf025277b5
chrte707 <chrte707@student.liu.se>
2013-01-22 15:42:41 +0100

    Commented the code

--- src/com/google/gwt/sample/stockwatcher/client/FlexTableDragController.java
+++ src/com/google/gwt/sample/stockwatcher/client/FlexTableDragController.java
@@ -7,91 +7,82 @@ import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Widget;
 
-
-
-
+/**
+ * Class for handling the DragController, inspired by https://gwt-dnd.appspot.com
+ * @author chrte707, hento581
+ *
+ */
 public final class FlexTableDragController extends PickupDragController{
-	
-	 private static final String CSS_DEMO_FLEX_TABLE_ROW_EXAMPLE_TABLE_PROXY = "demo-FlexTableRowExample-table-proxy";
+	private static final String CSS_DEMO_FLEX_TABLE_ROW_EXAMPLE_TABLE_PROXY = "demo-FlexTableRowExample-table-proxy";
 
 	@Override
 	public void dragStart(){
-		
+
 		super.dragStart();  
 	}
 
-	  private FlexTable draggableTable;
+	private FlexTable draggableTable;
+	private int dragRow;
 
-	  private int dragRow;
+	public FlexTableDragController(AbsolutePanel boundaryPanel) {
+		super(boundaryPanel, false);
+		setBehaviorDragProxy(true);
+		setBehaviorMultipleSelection(false);
+	}
 
-	  public FlexTableDragController(AbsolutePanel boundaryPanel) {
-	    super(boundaryPanel, false);
-	    setBehaviorDragProxy(true);
-	    setBehaviorMultipleSelection(false);
-	  }
+	@Override
+	public void dragEnd() {
+		super.dragEnd();
 
-	  @Override
-	  public void dragEnd() {
-	    super.dragEnd();
+		// cleanup
+		draggableTable = null;
+	}
 
-	    // cleanup
-	    draggableTable = null;
-	  }
+	@Override
+	public void setBehaviorDragProxy(boolean dragProxyEnabled) {
+		if (!dragProxyEnabled) {
+			throw new IllegalArgumentException();
+		}
+		super.setBehaviorDragProxy(dragProxyEnabled);
+	}
 
-	  @Override
-	  public void setBehaviorDragProxy(boolean dragProxyEnabled) {
-	    if (!dragProxyEnabled) {
-	      // TODO implement drag proxy behavior
-	      throw new IllegalArgumentException();
-	    }
-	    super.setBehaviorDragProxy(dragProxyEnabled);
-	  }
+	@Override
+	protected BoundaryDropController newBoundaryDropController(AbsolutePanel boundaryPanel,
+			boolean allowDroppingOnBoundaryPanel) {
+		if (allowDroppingOnBoundaryPanel) {
+			throw new IllegalArgumentException();
+		}
+		return super.newBoundaryDropController(boundaryPanel, allowDroppingOnBoundaryPanel);
+	}
 
-	  @Override
-	  protected BoundaryDropController newBoundaryDropController(AbsolutePanel boundaryPanel,
-	      boolean allowDroppingOnBoundaryPanel) {
-	    if (allowDroppingOnBoundaryPanel) {
-	      throw new IllegalArgumentException();
-	    }
-	    return super.newBoundaryDropController(boundaryPanel, allowDroppingOnBoundaryPanel);
-	  }
+	@Override
+	protected Widget newDragProxy(DragContext context) {
+		FlexTable proxy;
+		proxy = new FlexTable();
+		proxy.addStyleName(CSS_DEMO_FLEX_TABLE_ROW_EXAMPLE_TABLE_PROXY);
+		draggableTable = (FlexTable) context.draggable.getParent();
+		dragRow = getWidgetRow(context.draggable, draggableTable);
+		FlexTableUtil.copyRow(draggableTable, proxy, dragRow, 0);
+		return proxy;
+	}
 
-	  @Override
-	  protected Widget newDragProxy(DragContext context) {
-	    FlexTable proxy;
-	    proxy = new FlexTable();
-	    proxy.addStyleName(CSS_DEMO_FLEX_TABLE_ROW_EXAMPLE_TABLE_PROXY);
-	    draggableTable = (FlexTable) context.draggable.getParent();
-	    dragRow = getWidgetRow(context.draggable, draggableTable);
-	    FlexTableUtil.copyRow(draggableTable, proxy, dragRow, 0);
-	    return proxy;
-	  }
+	FlexTable getDraggableTable() {
+		return draggableTable;
+	}
 
-	  FlexTable getDraggableTable() {
-	    return draggableTable;
-	  }
+	int getDragRow() {
+		return dragRow;
+	}
 
-	  int getDragRow() {
-	    return dragRow;
-	  }
-
-	  private int getWidgetRow(Widget widget, FlexTable table) {
-	    for (int row = 0; row < table.getRowCount(); row++) {
-	      for (int col = 0; col < table.getCellCount(row); col++) {
-	        Widget w = table.getWidget(row, col);
-	        if (w == widget) {
-	          return row;
-	        }
-	      }
-	    }
-	    throw new RuntimeException("Unable to determine widget row");
-	  }
-
-	
-	
-
-	
-	
-
-
+	private int getWidgetRow(Widget widget, FlexTable table) {
+		for (int row = 0; row < table.getRowCount(); row++) {
+			for (int col = 0; col < table.getCellCount(row); col++) {
+				Widget w = table.getWidget(row, col);
+				if (w == widget) {
+					return row;
+				}
+			}
+		}
+		throw new RuntimeException("Unable to determine widget row");
+	}
 }
--- src/com/google/gwt/sample/stockwatcher/client/FlexTableDropController.java
+++ src/com/google/gwt/sample/stockwatcher/client/FlexTableDropController.java
@@ -14,104 +14,107 @@ import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 
+/**
+ * A class for handling the dropcontroller, inspired by https://gwt-dnd.appspot.com
+ * @author chrte707, hento581
+ *
+ */
 public class FlexTableDropController extends AbstractPositioningDropController  {
-	TemperatureWatcher parent;
-
+	@SuppressWarnings("unused")
+	private TemperatureWatcher parent;
 
 	private static final String CSS_DEMO_TABLE_POSITIONER = "demo-table-positioner";
+	private FlexTable flexTable;
+	private InsertPanel flexTableRowsAsIndexPanel = new InsertPanel() {
 
+		@Override
+		public void add(Widget w) {
+			throw new UnsupportedOperationException();
+		}
 
-	  private FlexTable flexTable;
+		@Override
+		public Widget getWidget(int index) {
+			return flexTable.getWidget(index, 0);
+		}
 
+		@Override
+		public int getWidgetCount() {
+			return flexTable.getRowCount();
+		}
+
+		@Override
+		public int getWidgetIndex(Widget child) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void insert(Widget w, int beforeIndex) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean remove(int index) {
+			throw new UnsupportedOperationException();
+		}
+	};
+
+	private Widget positioner = null;
+	private int targetRow;
 	
-	  private InsertPanel flexTableRowsAsIndexPanel = new InsertPanel() {
+	public FlexTableDropController(FlexTable flexTable) {
+		super(flexTable);
+		this.flexTable = flexTable;
+	}
 
-		    @Override
-		    public void add(Widget w) {
-		      throw new UnsupportedOperationException();
-		    }
+	@Override
+	public void onDrop(DragContext context) {
+		FlexTableDragController trDragController = (FlexTableDragController) context.dragController;
+		if(targetRow==-1){
+			targetRow=0;
+		}
+		FlexTableUtil.moveRow(trDragController.getDraggableTable(), flexTable,
+				trDragController.getDragRow(), targetRow + 1);
+		super.onDrop(context);
+	}
 
-		    @Override
-		    public Widget getWidget(int index) {
-		      return flexTable.getWidget(index, 0);
-		    }
+	@Override
+	public void onEnter(DragContext context) {
+		super.onEnter(context);
+		positioner = newPositioner(context);
+	}
 
-		    @Override
-		    public int getWidgetCount() {
-		      return flexTable.getRowCount();
-		    }
+	@Override
+	public void onLeave(DragContext context) {
+		positioner.removeFromParent();
+		positioner = null;
+		super.onLeave(context);
+	}
 
-		    @Override
-		    public int getWidgetIndex(Widget child) {
-		      throw new UnsupportedOperationException();
-		    }
+	@Override
+	/**
+	 * Functions for handling the onMove actions
+	 */
+	public void onMove(DragContext context) {
+		super.onMove(context);
+		targetRow = DOMUtil.findIntersect(flexTableRowsAsIndexPanel, new CoordinateLocation(
+				context.mouseX, context.mouseY), LocationWidgetComparator.BOTTOM_HALF_COMPARATOR) - 1;
 
-		    @Override
-		    public void insert(Widget w, int beforeIndex) {
-		      throw new UnsupportedOperationException();
-		    }
+		if (flexTable.getRowCount() > 0) {
+			Widget w = flexTable.getWidget(targetRow == -1 ? 0 : targetRow, 0);
+			Location widgetLocation = new WidgetLocation(w, context.boundaryPanel);
+			Location tableLocation = new WidgetLocation(flexTable, context.boundaryPanel);
+			context.boundaryPanel.add(positioner, tableLocation.getLeft(), widgetLocation.getTop()
+					+ (targetRow == -1 ? 0 : w.getOffsetHeight()));
+		}
+	}
 
-		    @Override
-		    public boolean remove(int index) {
-		      throw new UnsupportedOperationException();
-		    }
-		  };
+	Widget newPositioner(DragContext context) {
+		Widget p = new SimplePanel();
+		p.addStyleName(CSS_DEMO_TABLE_POSITIONER);
+		p.setPixelSize(flexTable.getOffsetWidth(), 1);
+		return p;
+	}
 
-		  private Widget positioner = null;
 
-		  private int targetRow;
 
-		  public FlexTableDropController(FlexTable flexTable) {
-		    super(flexTable);
-		    this.flexTable = flexTable;
-		  }
-
-		  @Override
-		  public void onDrop(DragContext context) {
-		    FlexTableDragController trDragController = (FlexTableDragController) context.dragController;
-		    if(targetRow==-1){
-		    	targetRow=0;
-		    }
-		    FlexTableUtil.moveRow(trDragController.getDraggableTable(), flexTable,
-		        trDragController.getDragRow(), targetRow + 1);
-		    super.onDrop(context);
-		  }
-
-		  @Override
-		  public void onEnter(DragContext context) {
-		    super.onEnter(context);
-		    positioner = newPositioner(context);
-		  }
-
-		  @Override
-		  public void onLeave(DragContext context) {
-		    positioner.removeFromParent();
-		    positioner = null;
-		    super.onLeave(context);
-		  }
-
-		  @Override
-		  public void onMove(DragContext context) {
-		    super.onMove(context);
-		    targetRow = DOMUtil.findIntersect(flexTableRowsAsIndexPanel, new CoordinateLocation(
-		        context.mouseX, context.mouseY), LocationWidgetComparator.BOTTOM_HALF_COMPARATOR) - 1;
-
-		    if (flexTable.getRowCount() > 0) {
-		      Widget w = flexTable.getWidget(targetRow == -1 ? 0 : targetRow, 0);  //TODO, cause of the nullpointer, w can be null
-		      Location widgetLocation = new WidgetLocation(w, context.boundaryPanel);
-		      Location tableLocation = new WidgetLocation(flexTable, context.boundaryPanel);
-		      context.boundaryPanel.add(positioner, tableLocation.getLeft(), widgetLocation.getTop()
-		          + (targetRow == -1 ? 0 : w.getOffsetHeight()));  //TODO: fix nullpointer exception here
-		    }
-		  }
-
-		  Widget newPositioner(DragContext context) {
-		    Widget p = new SimplePanel();
-		    p.addStyleName(CSS_DEMO_TABLE_POSITIONER);
-		    p.setPixelSize(flexTable.getOffsetWidth(), 1);
-		    return p;
-		  }
-
-	
-	
 }
